b894c16fdf3d599b7cac3efd5502935461e4ade4
sidbird <123426974+sidbird@users.noreply.github.com>
2023-05-08 19:11:01 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ rightWristX = 0;
 rightWristY = 0;
 song1 = "";
 song2 = "";
+
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
 function preload (){
     song1 = loadSound("music.mp3");
     song2 = loadSound("music2.mp3");
@@ -23,9 +26,13 @@ function setup(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        scoreLeftWrist= results[0].pose.keypoints[9].score;
+        console.log("socreLeftWrist = " + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX =" + leftWristX +"leftWristY ="+leftWristY);
+        
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("rightWristX =" + rightWristX +"rightWristY ="+rightWristY);
@@ -39,6 +46,20 @@ function modelLoaded(){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+    fill("#FF0000");
+    stroke("#FF0000");
+    if(scoreLeftWrist > 0.2){
+        circle(leftWristX, leftWristY, 20);
+
+        song1.stop();
+
+        if(song2_status == false)
+        {
+            song2.play();
+            document.getElementById("song").innerHTMl = "Playing - Peter Pan Song";
+        }
+
+}
 }
 
 function play1(){
